7b537f5b1f24a7a951f3c7dce1419de9e4f04e8d
Wesner M <wesnerm@gmail.com>
2022-11-12 15:02:38 -0500

    I dont know if my last commit went through but if it didnt my message was that this was my second commit with verbose comments

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-script();
-
-const petName = document.getElementbyID("petName");
-petName.innerHTML = `Name: ${event.currentTarget.petName.value}`;
-div.appendChild(petName);
+function name() {
+  Alert("testing");
+}
